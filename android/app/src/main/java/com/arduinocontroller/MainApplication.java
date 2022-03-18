d30b77030bd8033317718ea72c14c727795db282
unknown <gesang@merindo.co.id>
2022-03-18 09:33:54 +0700

    Fixing Running on android

--- android/app/src/main/java/com/arduinocontroller/MainApplication.java
+++ android/app/src/main/java/com/arduinocontroller/MainApplication.java
@@ -1,5 +1,7 @@
 package com.arduinocontroller;
 
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -29,6 +31,11 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() { 
+          return new ReanimatedJSIModulePackage(); 
         }
 
         @Override
